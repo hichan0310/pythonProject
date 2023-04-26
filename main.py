71ad623a37b352ca4c8235613e235f7225b21f94
maxma <maxmath0310@gmail.com>
2023-04-26 20:53:18 +0900

    Changes

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a=np.random.random(100)
-b=a*2+(np.random.randn(100)-0.5)*0.1
-p=0
+b=a*2+(np.random.randn(100)-0.5)*0.1+2
+p=[0, 0]
 lr=0.01
 plt.scatter(a, b)
 
 for i in range(1000):
     cost=0
     for j in range(100):
-        costt=a[j]*p-b[j]
+        costt=a[j]*p[0]+p[1]-b[j]
         cost+=costt
-        p -= costt*a[j]*lr
+        p[0] -= costt*a[j]*lr
+        p[1] -= costt*lr
     print(cost/100)
 
-plt.plot([0, 1], [0, p])
+plt.plot([0, 1], [p[1], p[0]+p[1]])
 plt.show()
 
 print(p)
